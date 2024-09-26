64a4fdb1502a7ec7c586e644ac5c13df4933c2cf
Lauty Cortez <lautycortez725@gmail.com>
2024-09-26 14:23:03 -0300

    Componentes Nuevos

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  
 }
+localStorage.setItem('userRole', 'paciente'); // o 'medico', 'operador', 'admin'
---
+++ src/app/components/operador/gestionar-pacientes-turnos/gestionar-pacientes-turnos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-pacientes-turnos',
+  templateUrl: './gestionar-pacientes-turnos.component.html',
+  styleUrls: ['./gestionar-pacientes-turnos.component.css']
+})
+export class GestionarPacientesTurnosComponent {
+
+}
