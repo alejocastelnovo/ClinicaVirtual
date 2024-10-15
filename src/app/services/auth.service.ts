c001ac783d5ee88836638135d5802cf5149527da
LautyCortez <gonzagonza613@gmail.com>
2024-10-15 15:02:23 -0300

    Back incompleto

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -7,12 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AuthService {
   private usuarios: any[] = [
-    { id: 1, nombre: 'Lautaro', apellido: 'Cortez', email: 'lautycortez725@gmail.com', password: '1234', dni: '3425458005', userType: 'Administrador', fechaNacimiento: '2003-12-23' },
-    { id: 2, nombre: 'Alejo', apellido: 'Castelnovo', email: 'castelnovo12@gmail.com', password: '123', dni: '0987654321', userType: 'Administrador', fechaNacimiento: '2001-2-12' },
-    { id: 3, nombre: 'Juan', apellido: 'Perez', email: 'juanperez@gmail.com', password: '123', dni: '1234567890', userType: 'Paciente', fechaNacimiento: '1990-05-15' },
-    { id: 4, nombre: 'Maria', apellido: 'Gomez', email: 'mariagomez@gmail.com', password: '123', dni: '0987654321', userType: 'Medico', fechaNacimiento: '1985-09-22' },
-    { id: 5, nombre: 'Carlos', apellido: 'Lopez', email: 'carloslopez@gmail.com', password: '123', dni: '1234567890', userType: 'Operador', fechaNacimiento: '1978-11-30' },
-  ];
+    ];
   UrlApi = 'http://localhost:4000/api';
   private usuarioLogueado: any = null;
   private usuarioLogueadoSubject = new BehaviorSubject<any>(null);
@@ -21,9 +17,12 @@ export class AuthService {
 
   login(body: any) {
     const headers = new HttpHeaders({ "Content-Type": "application/json" });
-    
-  
-    return this.http.post(`${this.UrlApi}/login`, body, {headers})
+    return this.http.post(`${this.UrlApi}/login`, body, {headers}).pipe(
+      tap((response: any) => {
+        this.usuarioLogueado = response.usuario;
+        this.usuarioLogueadoSubject.next(this.usuarioLogueado);
+      })
+    );
   }
 
   logout(): void {
@@ -31,76 +30,48 @@ export class AuthService {
     this.usuarioLogueadoSubject.next(null);
   }
 
-  registrarUsuario(usuario: any): boolean {
-    usuario.id = this.usuarios.length + 1;
-    this.usuarios.push(usuario);
-    return true;
+  registrarUsuario(usuario: any): Observable<any> {
+    return this.http.post(`${this.UrlApi}/usuarios`, usuario);
   }
 
-  getUsuarioLogueado() {
+  getUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.UrlApi}/usuarios`);
+  }
+
+  crearUsuario(usuario: any): Observable<any> {
+    return this.http.post(`${this.UrlApi}/usuarios`, usuario);
+  }
+
+  editarUsuario(id: number, usuarioEditado: any): Observable<any> {
+    return this.http.put(`${this.UrlApi}/usuarios/${id}`, usuarioEditado);
+  }
+
+  getUsuarioPorId(id: number): Observable<any> {
+    return this.http.get(`${this.UrlApi}/usuarios/${id}`);
+  }
+
+  eliminarUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.UrlApi}/usuarios/${id}`);
+  }
+
+  // Método para obtener el usuario logueado
+  getUsuarioLogueado(): any {
     return this.usuarioLogueado;
   }
 
+  // Método para obtener el Observable del usuario logueado
   getUsuarioLogueadoObservable(): Observable<any> {
     return this.usuarioLogueadoSubject.asObservable();
   }
 
-  esUsuarioAdministrador(): boolean {
-    return this.usuarioLogueado && this.usuarioLogueado.userType === 'Administrador';
-  }
-
-  verificarUsuarioExistente(email: string): boolean {
-    // Buscar un usuario con el email proporcionado
-    const usuarioExistente = this.usuarios.find(u => u.email === email);
-    
-    // Retornar true si se encuentra un usuario, false en caso contrario
-    return !!usuarioExistente;
-  }
-
-  actualizarDatosUsuario(id: number, cambios: any): boolean {
-    const index = this.usuarios.findIndex(u => u.id === id);
-    if (index !== -1) {
-      this.usuarios[index] = { ...this.usuarios[index], ...cambios };
-      if (this.usuarioLogueado && this.usuarioLogueado.id === id) {
-        this.usuarioLogueado = { ...this.usuarioLogueado, ...cambios };
+  // Método para actualizar los datos del usuario
+  actualizarDatosUsuario(id: number, cambios: any): Observable<any> {
+    return this.http.put(`${this.UrlApi}/usuarios/${id}`, cambios).pipe(
+      tap((usuarioActualizado: any) => {
+        this.usuarioLogueado = usuarioActualizado;
         this.usuarioLogueadoSubject.next(this.usuarioLogueado);
-      }
-      return true;
-    }
-    return false;
-  }
-
-  getUsuarios(): Observable<any[]> {
-    return of(this.usuarios);
-  }
-
-  crearUsuario(usuario: any): Observable<boolean> {
-    usuario.id = this.usuarios.length + 1;
-    this.usuarios.push(usuario);
-    return of(true);
-  }
-
-  editarUsuario(id: number, usuarioEditado: any): Observable<boolean> {
-    const index = this.usuarios.findIndex(u => u.id === id);
-    if (index !== -1) {
-      this.usuarios[index] = { ...this.usuarios[index], ...usuarioEditado };
-      return of(true);
-    }
-    return of(false);
-  }
-
-  getUsuarioPorId(id: number): Observable<any> {
-    const usuario = this.usuarios.find(u => u.id === id);
-    return of(usuario);
-  }
-
-  eliminarUsuario(id: number): Observable<boolean> {
-    const index = this.usuarios.findIndex(u => u.id === id);
-    if (index !== -1) {
-      this.usuarios.splice(index, 1);
-      return of(true);
-    }
-    return of(false);
+      })
+    );
   }
 
 }
