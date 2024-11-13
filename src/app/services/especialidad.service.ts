1f43db096a26354698616fb0a7c581643011049f
Lautaro Cortez <lautycortez725@gmail.com>
2024-11-13 19:41:56 -0300

    merge 2

--- src/app/services/especialidad.service.ts
+++ src/app/services/especialidad.service.ts
@@ -3,21 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class EspecialidadService {
 
   private apiUrl = 'http://localhost:4000/api';  // La URL del backend
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
     private getHeaders(): HttpHeaders {
         const token = localStorage.getItem('jwt');
         return new HttpHeaders({
-            'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
             'Authorization': token || ''
-        });
-    return this.http.get(`${this.apiUrl}/obtenerEspecialidadesMedico/${id}`, { headers });
-    }
+    });
+  }
 
   obtenerEspecialidades(token:string): Observable<any> {
     const headers = new HttpHeaders({
@@ -25,7 +24,7 @@ export class EspecialidadService {
       'authorization': token
     });
     return this.http.get(`${this.apiUrl}/obtenerEspecialidades`, { headers });
-    }
+  }
 
   crearMedicoEspecialidad(body: any, token: string): Observable<any> {
     const headers = new HttpHeaders({
@@ -33,7 +32,7 @@ export class EspecialidadService {
       'authorization': token
     });
     return this.http.post(`${this.apiUrl}/crearMedicoEspecialidad`, body ,{ headers });
-    }
+  }
 
   obtenerCobertura(token: string): Observable<any> {
     const headers = new HttpHeaders({
@@ -49,5 +48,5 @@ export class EspecialidadService {
       'authorization': token
     });
     return this.http.get(`${this.apiUrl}/obtenerMedicoPorEspecialidad/${id_especialidad}` ,{ headers });
-    }
+  }
 }
